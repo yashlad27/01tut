12f37b88eb003afccf8bd34687ced1396102f6d5
Yash Lad <yashlad727@gmail.com>
2022-11-26 22:57:43 +0530

    adding styling to app.js and creating new grocery list component

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Header = () => {
+  const headerStyle = {
+    backgroundColor: 'mediumseagreen',
+    color: '#fff'
+  };
+
   return (
     // <div>Header</div>
+    // <header style={headerStyle}>
     <header>
         <h1>Groceries List</h1>
     </header>
