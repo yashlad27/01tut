55343a97d8733f6c8699e7bb64e84738c6e1be6b
Yash Lad <yashlad727@gmail.com>
2022-11-26 22:13:21 +0530

    adding user-defined functions

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+  
   const name = 'John Doe';
-  return (
+
+  const handleNameChange = () => {
+    const names = ['John Doe', 'Jane Doe', 'Joe Doe'];
+    const int  = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to see changes.
+          {/* Hello {name}; */}
+          Hello {handleNameChange()}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
         <p>{name}</p>
+        <p>{[1, 2, 3]}</p>
+        <p>[1, 2, 3]</p>
+        {/* <p>{name}</p> */}
       </header>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    // <div>Header</div>
+    <header>
+        <h1>Groceries List</h1>
+    </header>
+  )
+}
+
+export default Header
