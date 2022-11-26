7da8019d1c286d705720d0da6f53331745e46584
Yash Lad <yashlad727@gmail.com>
2022-11-26 23:07:23 +0530

    anonymous functions and vanilla js

--- src/Content.js
+++ src/Content.js
@@ -9,11 +9,25 @@ const Content = () => {
       return names[int];
     }
 
+    const handleClick = () => {
+        console.log('You Clicked it')
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target)
+    }
   return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click It</button>
+        <button onClick={() => handleClick2('John')}>Click It</button>
+        <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   )
 }
